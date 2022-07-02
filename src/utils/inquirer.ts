b6bd87886375acf89450326a3a079ceaefcc7623
Jawkx <j_kaixuan@live.com>
2022-07-03 01:50:19 +0800

    Add auto complete prompt

--- src/utils/inquirer.ts
+++ src/utils/inquirer.ts
@@ -1,6 +1,8 @@
 import inquirer from "inquirer";
 import PressToContinuePrompt from "inquirer-press-to-continue";
+import AutocompletePrompt from "inquirer-autocomplete-prompt";
 
 inquirer.registerPrompt("press-to-continue", PressToContinuePrompt);
+inquirer.registerPrompt("autocomplete", AutocompletePrompt);
 
 export default inquirer;
